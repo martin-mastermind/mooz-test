8062ed7e58d559b68be2699a8888ef663b2e576d
Martin <martbelmoaw@gmail.com>
2024-06-12 13:41:47 +0300

    feat: add search componen

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+import BaseSearch from "./components/BaseSearch/BaseSearch.vue";
+
+const query = ref("");
+</script>
 
 <template>
-  <div>Hello world!</div>
+  <main>
+    <BaseSearch v-model="query" />
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/BaseSearch/BaseSearch.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const modelValue = defineModel("modelValue", {
+  default: "",
+});
+</script>
+
+<template>
+  <input
+    type="text"
+    v-model="modelValue"
+    placeholder="Search"
+    class="base-search"
+  />
+</template>
+
+<style scoped>
+.base-search {
+  width: 100%;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+}
+</style>
